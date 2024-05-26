677d8f40569280a67c32e324e4813216b8dfa138
Fahad <205fahadmohammad@gmail.com>
2024-05-26 22:06:11 +0600

    feat(products) delete product functionality add

--- src/pages/dashboard/AllProducts.jsx
+++ src/pages/dashboard/AllProducts.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
+import Swal from "sweetalert2";
 
 const AllProducts = () => {
   const [watches, setWatches] = useState([]);
@@ -14,6 +15,36 @@ const AllProducts = () => {
     }
     loadData();
   }, []);
+
+  const handleDelete = async (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const data = await axios.delete(`http://localhost:3000/watches/${id}`);
+
+        if (data?.status === 200) {
+          Swal.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success",
+          });
+        } else {
+          Swal.fire({
+            title: "Failed!",
+            text: "Product failed to delete.",
+            icon: "error",
+          });
+        }
+      }
+    });
+  };
   return (
     <div className="overflow-x-auto">
       <table className="table">
@@ -33,7 +64,7 @@ const AllProducts = () => {
               <td>
                 <div className="flex items-center gap-3">
                   <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
+                    <div className="mask mask-squircle w-14 h-14">
                       <img src={watch?.image} alt="watch" />
                     </div>
                   </div>
@@ -44,10 +75,17 @@ const AllProducts = () => {
               </td>
               <td>{watch.price}</td>
               <td>{watch.category}</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">update</button>
-                <button className="btn btn-ghost btn-xs">delete</button>
-              </th>
+              <td>
+                <button className="btn btn-ghost btn-xs bg-orange-200 lg:mr-2">
+                  update
+                </button>
+                <button
+                  onClick={() => handleDelete(watch.id)}
+                  className="btn btn-ghost btn-xs bg-red-400"
+                >
+                  delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
